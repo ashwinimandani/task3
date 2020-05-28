523e3bb931d1e51130764e0bbbdf98a6024d360a
Ashwini Mandani <51437138+ashwinimandani@users.noreply.github.com>
2020-05-28 21:07:39 +0530

    Update fashion_mnist.py

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -12,7 +12,6 @@ from tensorflow import keras
 import numpy as np
 import pandas as pd
 
-print(tf.__version__)
 
 fashion_mnist=keras.datasets.fashion_mnist
 
@@ -29,37 +28,46 @@ test_images=test_images.reshape(len(test_images),28,28,1)
 train_images[1].shape
 
 
+import imp
+f = open("/workspace/input.txt","r")
+global data
+data = imp.load_source('data', '', f)
+f.close()
+epochs= int(data.epochs)
 
 def build_model():
-  model=keras.Sequential([keras.layers.Conv2D(filters=64,
+  model=keras.Sequential([keras.layers.Conv2D(filters=32,
                                               kernel_initializer="he_normal",
                                               kernel_size=(3,3),
                                               activation="relu",
                                               input_shape=(28,28,1)),
-                          keras.layers.Conv2D(filters=32,
-                                              kernel_size=(5,5),
-                                              activation="relu",
                                               ),
                           keras.layers.Flatten(),
-                          keras.layers.Dense(units=64,activation="relu",kernel_initializer="he_normal"),
+                          keras.layers.Dense(units=32,activation="relu",kernel_initializer="he_normal"),
                           keras.layers.Dense(units=10,activation="softmax")
                           ])
   model.compile(keras.optimizers.Adam(0.001),loss="sparse_categorical_crossentropy",metrics=["accuracy"])
-
   return model
 
 model=build_model()
 
 model.summary()
 
-model.fit(train_images,train_labels,epochs=10,validation_split=0.001)
+model.fit(train_images,train_labels,epochs=epochs,validation_split=0.001)
 
 y_pred=model.predict(test_images)
 
-for i in range(len(y_pred)):
-  y_pred[i]=list(map(int,y_pred[i]))
+loss,accuracy=model.evaluate(train_images,train_labels)
 
+file=open("/workspace/accuracy.txt","w")
+file.write(str(accuracy))
+file.close()
 
+epochs=epochs +1
+epochs=str(epochs)
+str1="epochs= "+epochs
+f=open("/workspace/input.txt","w")
+file.write(str1)
 
 
 
